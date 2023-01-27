38005a109e766bfb67b5c6575525a3dadcd69530
SlavaMSV <viacheslav.maslov@gmail.com>
2023-01-27 19:26:53 +0300

    Добавлена программа нахождения максимального из трех чисел

---
+++ ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача нахождения максимального из трех чисел
+Console.WriteLine("Введите 3 целых числа");
+string astr = Console.ReadLine();
+string bstr = Console.ReadLine();
+string cstr = Console.ReadLine();
+int a = Convert.ToInt32(astr);
+int b = Convert.ToInt32(bstr);
+int c = Convert.ToInt32(cstr);
+int max = 0;
+if ((a > b) && (a > c))
+max = a;
+else if ((b > a) && (b > c))
+max = b;
+else if ((c > a) && (c > b))
+max = c;
+Console.WriteLine($"максимальное число: {max}");
