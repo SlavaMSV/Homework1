7d0d755d8a3f2f56a719486c2bb31b6db9eca8f9
SlavaMSV <viacheslav.maslov@gmail.com>
2023-01-27 19:05:15 +0300

    Добавлена программа нахождения максимального из двух чисел

---
+++ ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача нахождения максимального из двух чисел
+Console.WriteLine("Введите 2 числа:");
+string astr = Console.ReadLine();
+string bstr = Console.ReadLine();
+int a = Convert.ToInt32(astr);
+int b = Convert.ToInt32(bstr);
+if(a > b)
+Console.WriteLine($"max = {a}");
+else if(a < b)
+Console.WriteLine($"max = {b}");
+else if(a==b)
+Console.WriteLine("числа равны");
