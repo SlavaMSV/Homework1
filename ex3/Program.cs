471f44f63010d8334092ba379cb7cfabc376b91c
SlavaMSV <viacheslav.maslov@gmail.com>
2023-01-27 19:51:27 +0300

    добавлена программа проверки четности числа

---
+++ ex3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа проверки четности числа
+Console.WriteLine("Введите число:");
+string nstr = Console.ReadLine();
+int n = Convert.ToInt32(nstr);
+if(n % 2 == 0)
+Console.WriteLine($"{n} - четное число");
+else 
+Console.WriteLine($"{n} - нечетное число");
