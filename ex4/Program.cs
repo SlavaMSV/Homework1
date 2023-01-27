a49500a15dd94d6a74a50f416810cdae4c12ef65
SlavaMSV <viacheslav.maslov@gmail.com>
2023-01-27 21:10:11 +0300

    добавлена программа нахождения четных чисел в заданном диапозоне

---
+++ ex4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа показа четных чисел в диапозоне от 1 до N
+Console.WriteLine("Введите положительное число:");
+string nstr = Console.ReadLine();
+int n = Convert.ToInt32(nstr);
+int i = 2;
+while(i <= n)
+{
+    Console.Write($"{i} ");
+    i+=2;
+}
+
